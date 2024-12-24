9c9802b83e40433be00954f144cb57001f5a43ce
Sheyyma <144834962+Sheyyma@users.noreply.github.com>
2024-12-24 19:38:13 +0300

    FireBase Done

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:ai_bot_test/Page/CameraPage.dart';
 import 'package:ai_bot_test/Page/MainPage.dart';
 import 'package:ai_bot_test/Page/chatBotPage.dart';
 import 'package:ai_bot_test/Theme/theme_provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:ai_bot_test/navBar.dart';
 import 'package:provider/provider.dart';
@@ -18,33 +20,54 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  //String Tanımlamalar
-  final String appbarString = 'Cifci Dostu ChatBot';
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'BMT Proje',
       theme: Provider.of<ThemeProvider>(context).themeData,
-      home: MyHomePage(title: appbarString),
+      home: const RootPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class RootPage extends StatefulWidget {
+  const RootPage({super.key});
 
-  final String title;
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  bool isLoggedIn = false;
+
+  void login() {
+    setState(() {
+      isLoggedIn = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return isLoggedIn ? const MyHomePage() : LoginScreen(onLogin: login);
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  // Initialize Firebase App
+  Future<FirebaseApp> _initializeFirebase() async {
+    return await Firebase.initializeApp();
+  }
+
   int currentIndex = 1;
-  List<Widget> pages = [
+  final List<Widget> pages = [
     Mainpage(),
     Camerapage(),
     Chatbotpage(),
@@ -52,45 +75,159 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initializeFirebase(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Scaffold(
+            drawer: const Navbar(),
+            appBar: AppBar(
+              title: const Text(
+                'Çiftçi Dostu ChatBot',
+                style: TextStyle(
+                  fontFamily: 'SemiJoined',
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            body: pages[currentIndex],
+            bottomNavigationBar: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              iconSize: 28,
+              showUnselectedLabels: false,
+              currentIndex: currentIndex,
+              onTap: (index) => setState(() => currentIndex = index),
+              items: const [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home_filled),
+                  label: "Home",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.camera_alt),
+                  label: "Camera Page",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.chat_bubble),
+                  label: "ChatBot",
+                )
+              ],
+            ),
+          );
+        }
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  final VoidCallback onLogin;
+
+  const LoginScreen({Key? key, required this.onLogin}) : super(key: key);
+
+  // Login function
+  static Future<User?> loginUsingEmailPassword({
+    required String email,
+    required String password,
+    required BuildContext context,
+  }) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user;
+    try {
+      UserCredential userCredential = await auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      user = userCredential.user;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == "user-not-found") {
+        print("No User Found For That email");
+      }
+    }
+    return user;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController _emailController = TextEditingController();
+    TextEditingController _passwordController = TextEditingController();
+
     return Scaffold(
-      drawer: const Navbar(),
-      appBar: AppBar(
-        //backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(
-          //Başlıktaki text yazısı data'ları
-          widget.title,
-          style: const TextStyle(
-            fontFamily: 'SemiJoined',
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
-          ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text("Çiftçim",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold,
+                )),
+            const Text(
+              "Login to Your App",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 44.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 44.0),
+            TextField(
+              controller: _emailController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                hintText: "User Email",
+                prefixIcon: Icon(Icons.mail, color: Colors.black),
+              ),
+            ),
+            const SizedBox(height: 26.0),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(
+                hintText: "User Password",
+                prefixIcon: Icon(Icons.security, color: Colors.black),
+              ),
+            ),
+            const SizedBox(height: 8.0),
+            const Text(
+              "Don't Remember Your Password?",
+              style: TextStyle(color: Colors.blue),
+            ),
+            const SizedBox(height: 88.0),
+            SizedBox(
+              width: double.infinity,
+              child: RawMaterialButton(
+                fillColor: const Color(0xFF0069FE),
+                elevation: 0.0,
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12.0)),
+                onPressed: () async {
+                  User? user = await loginUsingEmailPassword(
+                    email: _emailController.text,
+                    password: _passwordController.text,
+                    context: context,
+                  );
+                  if (user != null) {
+                    onLogin();
+                  }
+                },
+                child: const Text(
+                  "Login",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
-      ),
-      body: pages[currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        //unselectedItemColor: Colors.grey,
-        // backgroundColor: Colors.white,
-        // selectedItemColor: Colors.green[800],
-        iconSize: 28,
-        showUnselectedLabels: false,
-        currentIndex: currentIndex,
-        onTap: (index) => setState(() => currentIndex = index),
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home_filled),
-            label: "Home",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.camera_alt),
-            label: "Camera Page",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.chat_bubble),
-            label: "ChatBot",
-          )
-        ],
       ),
     );
   }
